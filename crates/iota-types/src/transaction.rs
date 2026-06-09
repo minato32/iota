e83306554ab4f56a70ddb781262e26985ea549c0
Luigi <luigi.vigneri@iota.org>
2026-06-09 18:44:33 +0200

    chore: take &InputObjects in check_coin_deny_list_v1

--- crates/iota-types/src/transaction.rs
+++ crates/iota-types/src/transaction.rs
@@ -2998,12 +2998,6 @@ impl CheckedInputObjects {
         Self(input_objects)
     }
 
-    // Only called by post-consensus coin deny-list re-checks for attested
-    // transactions.
-    pub fn new_for_deny_list_check_only(input_objects: InputObjects) -> Self {
-        Self(input_objects)
-    }
-
     pub fn inner(&self) -> &InputObjects {
         &self.0
     }
